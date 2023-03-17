b4b5f8f0dcf166137aec3eadbf50e0474b3d989d
inveve <zachary@inveve.com>
2023-03-17 18:41:55 -0400

    Clean up

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,27 +8,27 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next Twitter Deez Nuts Joke in seconds."
           />
-          <meta property="og:site_name" content="twitterbio.com" />
+          <meta property="og:site_name" content="deeztweets.com/" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next Twitter Deez Nuts Joke in seconds."
           />
-          <meta property="og:title" content="Twitter Bio Generator" />
+          <meta property="og:title" content="Twitter Deez Nuts Joke" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
+          <meta name="twitter:title" content="Twitter Deez Nuts Joke" />
           <meta
             name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next Twitter Deez Nuts Joke in seconds."
           />
           <meta
             property="og:image"
-            content="https://twitterbio.com/og-image.png"
+            content="https://deeztweets.com/23772608_0.jpg"
           />
           <meta
             name="twitter:image"
-            content="https://twitterbio.com/og-image.png"
+            content="https://deeztweets.com/23772608_0.jpg"
           />
         </Head>
         <body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -106,7 +106,7 @@ const Home3: NextPage = () => {
     return (
         <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
             <Head>
-                <title>Twitter Reply Generator</title>
+                <title>Tweet Deez Generator</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
